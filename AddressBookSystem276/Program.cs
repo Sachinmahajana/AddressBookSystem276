ba338cccbd42c9659c40bd041121f0159e66752c
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-25 22:44:23 +0530

    [Sachin] Adding UC2-AddNewContact

--- AddressBookSystem276/Program.cs
+++ AddressBookSystem276/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Problem");
+            Console.WriteLine("\n1:Createcontact\n2:AddnewContact");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                //UC1 Createcontacts
+                case 1:
+                    Addressbook.CreateContacts();
+                    break;
+                //UC2 Addanewcontacts
+                case 2:
+                    Addressbook.CreateContacts();
+                    break;
+                default:
+                    Console.WriteLine("Choose from given option only");
+                    break;
+                     
+
+
+
+            }
         }
     }
 }
