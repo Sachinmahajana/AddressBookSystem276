3ccc710818b93db6ded77b3cf2dbe0e476cf45f4
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-25 23:19:25 +0530

    [Sachin] Adding UC3-EditContacts

--- AddressBookSystem276/Program.cs
+++ AddressBookSystem276/Program.cs
@@ -5,24 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Problem");
-            Console.WriteLine("\n1:Createcontact\n2:AddnewContact");
+            Console.WriteLine("\n1:Createcontact\n2:AddnewContact\n3:EditContacts");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 //UC1 Createcontacts
                 case 1:
-                    Addressbook.CreateContacts();
+                    AddressBook myObj = new AddressBook();
+                    myObj.CreateContacts();
                     break;
                 //UC2 Addanewcontacts
                 case 2:
-                    Addressbook.CreateContacts();
+                    AddressBook myObj1 = new AddressBook();
+                    myObj1.CreateContacts();
+                    break;
+                //UC3 Exit existing contact
+                case 3:
+                    AddressBook myObj2 = new AddressBook();
+                    myObj2.EditContact();
                     break;
                 default:
                     Console.WriteLine("Choose from given option only");
                     break;
-                     
-
-
 
             }
         }
